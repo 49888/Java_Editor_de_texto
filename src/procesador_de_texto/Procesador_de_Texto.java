531a114e8cd4606ad69ab30751b4566ca35a4c1a
1812 <francogadea1812@outlook.com.ar>
2020-07-15 00:50:10 -0300

    Primer Commit

---
+++ src/procesador_de_texto/Procesador_de_Texto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procesador_de_texto;
+
+/**
+ *
+ * @author 1812
+ */
+public class Procesador_de_Texto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
